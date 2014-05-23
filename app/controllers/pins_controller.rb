b47d17d4201447f322df71b883211b7b995752ff
Henry Huang <hhuang2088@gmail.com>
2014-05-22 23:40:35 -0700

    test

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,22 @@
+class PinsController < ApplicationController
+  def index
+    @pins = Pin.all 
+  end
+
+  def new 
+    @pin = Pin.new 
+  end
+
+  def create 
+    @pin = Pin.new(pin_params)
+    redirect_to new_pin_path
+  end
+
+  def show
+  end
+
+  private 
+  def pin_params 
+    params.require(:pin).permit(:longitude, :latitude)
+  end
+end
